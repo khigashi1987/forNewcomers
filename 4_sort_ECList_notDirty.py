87b780880ced5566b7adf853ff2b7f9de5c4c2cb
東 光一 <higashi@higashi-imac.local>
2014-04-30 13:05:43 +0900

    first commit

---
+++ 4_sort_ECList_notDirty.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
+#
+# Author:   HIGASHI Koichi
+# Created:  2014-03-14
+#
+ECs = []
+for line in open('./enzyme_randomized.list'):
+    EC = dict.fromkeys(['numbers', 'line'])
+    EC['line'] = line
+    EC['numbers'] = [ int(x) for x in line.rstrip().split()[0].split(':')[1].split('.') ]
+    ECs.append(EC)
+
+ECs.sort(cmp=lambda x,y:cmp(x['numbers'], y['numbers']))
+
+ofp = open('./enzyme_sorted.list','w')
+for ec in ECs:
+    ofp.write(ec['line'])
+ofp.close()
